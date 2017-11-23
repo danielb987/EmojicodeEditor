cb4c3bf2db1447873755d9962f126acf8a090126
Daniel <db123@bergqvist.se>
2017-11-23 13:36:03 +0100

    Make Travis generate Javadoc

---
+++ travis_utility/push-javadoc-to-docs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# https://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
+
+# if [ "$TRAVIS_REPO_SLUG" == "danielb987/EmojicodeEditor" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+if [ "$TRAVIS_REPO_SLUG" == "danielb987/EmojicodeEditor" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing javadoc...\n"
+
+  cp -R build/docs/javadoc $HOME/javadoc-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/danielb987/EmojicodeEditor docs > /dev/null
+
+  cd gh-pages
+  git rm -rf ./docs/javadoc
+  cp -Rf $HOME/javadoc-latest ./docs/javadoc
+  git add -f .
+  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to master"
+  git push -fq origin master > /dev/null
+
+  echo -e "Published Javadoc to gh-pages.\n"
+  
+fi
