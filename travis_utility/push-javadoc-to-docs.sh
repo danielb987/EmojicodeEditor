67085926e6681fcda2afd4500191a6a27e00025b
Daniel <db123@bergqvist.se>
2017-11-23 14:26:29 +0100

    Commit

--- travis_utility/push-javadoc-to-docs.sh
+++ travis_utility/push-javadoc-to-docs.sh
@@ -6,18 +6,24 @@
 
 if [ "$TRAVIS_REPO_SLUG" == "danielb987/EmojicodeEditor" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
-  echo -e "Publishing javadoc...\n"
+  echo -e "Generate javadoc...\n";
 
-  cp -R dist/javadoc $HOME/javadoc-latest
+#  cp -R dist/javadoc $HOME/javadoc-latest
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git clone --quiet --branch=master https://${GH_TOKEN}@github.com/danielb987/EmojicodeEditor docs > /dev/null
 
-  cd gh-pages
   git rm -rf ./docs/javadoc
-  cp -Rf $HOME/javadoc-latest ./docs/javadoc
+
+  ant javadoc
+
+  echo -e "Publishing javadoc...\n"
+
+  # cd gh-pages
+#  git rm -rf ./docs/javadoc
+#  cp -Rf $HOME/javadoc-latest ./docs/javadoc
   git add -f .
   git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to master"
   git push -fq origin master > /dev/null
