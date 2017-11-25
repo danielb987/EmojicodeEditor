f1df7ace25c5053779d02b7c8edafbe397cda487
Daniel <db123@bergqvist.se>
2017-11-25 08:31:03 +0100

    Commit

--- travis_utility/push-javadoc-to-docs---daniel.sh
+++ travis_utility/push-javadoc-to-docs---daniel.sh
@@ -4,11 +4,11 @@ if [ "$TRAVIS_REPO_SLUG" == "danielb987/EmojicodeEditor" ] && [ "$TRAVIS_PULL_RE
 
   echo -e "Run javadoc"
 
-  ant javadoc
+#  ant javadoc
 
   echo -e "Publishing javadoc...\n"
 
-  if [ false ]; then
+  if false ; then
 
   echo -e "Test AAA\n"
 
