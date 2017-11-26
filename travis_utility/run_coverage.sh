e8e7c276a5e26212b19fda0ca62c68b991b00c2a
Daniel <db123@bergqvist.se>
2017-11-26 20:10:19 +0100

    Commit

--- travis_utility/run_coverage.sh
+++ travis_utility/run_coverage.sh
@@ -4,29 +4,10 @@ if [ "$TRAVIS_REPO_SLUG" == "danielb987/EmojicodeEditor" ] && [ "$TRAVIS_PULL_RE
 
   echo -e "Run coverage"
 
-  wget http://search.maven.org/remotecontent?filepath=org/jacoco/jacoco/0.7.9/jacoco-0.7.9.zip -O jacoco-0.7.9.zip
+  wget http://search.maven.org/remotecontent?filepath=org/jacoco/jacoco/0.7.9/jacoco-0.7.9.zip -O jacoco-0.7.9.zip > /dev/null
 
-  echo -e "------------------"
-  ls
-  echo -e "------------------"
-  unzip jacoco-0.7.9.zip
-  echo -e "------------------"
-  ls
-  echo -e "------------------"
-  find
-  echo -e "------------------"
+  unzip jacoco-0.7.9.zip > /dev/null
 
   ant jacoco_rebuild
-
-#  cd jacoco
-
-#  pwd
-
-#  mkdir target
-
-#  ant clean
-#  ant compile
-#  ant test
-#  ant report
 
 fi
