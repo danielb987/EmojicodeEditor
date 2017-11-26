25b048b41718cca9067bdd69ce05a65453216766
Daniel <db123@bergqvist.se>
2017-11-26 12:47:20 +0100

    Commit

---
+++ travis_utility/run_coverage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "danielb987/EmojicodeEditor" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Run coverage"
+
+#  ant javadoc
+
+fi
