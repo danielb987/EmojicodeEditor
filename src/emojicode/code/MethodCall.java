2d58e101a58b34dba7078c4a3908a29925b46be9
Daniel <db123@bergqvist.se>
2017-12-04 08:46:22 +0100

    Fix checkstyle errors in MethodCall.java

--- src/emojicode/code/MethodCall.java
+++ src/emojicode/code/MethodCall.java
@@ -35,7 +35,7 @@ import java.util.List;
 /**
  * Emojicode method call.
  * http://www.emojicode.org/docs/reference/classes-valuetypes.html#srule-method-call
- * 
+ *
  * @author Daniel Bergqvist
  */
 public final class MethodCall extends Parent {
@@ -43,22 +43,22 @@ public final class MethodCall extends Parent {
     /**
      * The type of method.
      */
-    final EmojiMethod.MethodType methodType;
+    private final EmojiMethod.MethodType methodType;
     
     /**
      * The name of the method.
      */
-    String methodName;
+    private String methodName;
     
     /**
      * The name of the class that the method is declared in.
      */
-    String className;
+    private String className;
     
     /**
      * The emojicode class that the method is declared in.
      */
-    EmojiClass emojiClass;
+    private EmojiClass emojiClass;
     
 //    Parent.Variable variable;
     
@@ -66,22 +66,22 @@ public final class MethodCall extends Parent {
      * If this method call is on a constant string, the method call is for this string, or this
      * string is null.
      */
-    String constantString;
+    private String constantString;
     
     /**
      * The emojicode method that this method call expression should call.
      */
-    EmojiMethod emojiMethod;
+    private EmojiMethod emojiMethod;
     
     
     /**
      * Create a emojicode method call.
      * @param parent The parent
-     * @param methodType The type of method
+     * @param aMethodType The type of method
      */
-    public MethodCall(final Parent parent, final EmojiMethod.MethodType methodType) {
+    public MethodCall(final Parent parent, final EmojiMethod.MethodType aMethodType) {
         super(parent);
-        this.methodType = methodType;
+        this.methodType = aMethodType;
     }
     
     
@@ -105,31 +105,68 @@ public final class MethodCall extends Parent {
             constantString = classIdentifierToken.toString();
             this.middlePosition = classIdentifierToken.endPosition;
             this.endPosition = classIdentifierToken.endPosition;
-            emojiClass = EmojiPackage.packages.get("s").classes.get(EmojiString.getStringClassEmoji());
+            emojiClass = EmojiPackage
+                            .packages
+                                .get("s")
+                                    .classes
+                                        .get(EmojiString.getStringClassEmoji());
+            
             tokenStream.consumeToken();
-            if (emojiClass == null)
-                throw new CompilerError(classIdentifierToken.startPosition, classIdentifierToken.endPosition, String.format("Class %s in package s is not found", classIdentifierToken.toString()));
-            emojiMethod = emojiClass.getMethod(methodName, methodIdentifierToken.startPosition, classIdentifierToken.endPosition);
-            if (emojiMethod == null)
-                throw new CompilerError(methodIdentifierToken.startPosition, classIdentifierToken.endPosition, String.format("Method %s in class %s in package s is not found", methodName, classIdentifierToken.toString()));
+            
+            if (emojiClass == null) {
+                throw new CompilerError(classIdentifierToken.startPosition,
+                                        classIdentifierToken.endPosition,
+                                        String.format("Class %s in package s is not found",
+                                                      classIdentifierToken.toString()));
+            }
+            
+            emojiMethod = emojiClass.getMethod(methodName,
+                                               methodIdentifierToken.startPosition,
+                                               classIdentifierToken.endPosition);
+            
+            if (emojiMethod == null) {
+                throw new CompilerError(methodIdentifierToken.startPosition,
+                                        classIdentifierToken.endPosition,
+                                        String.format(
+                                                "Method %s in class %s in package s is not found",
+                                                methodName,
+                                                classIdentifierToken.toString()));
+            }
             
         } else if (classIdentifierToken.type == TokenType.Identifier) {
             tokenStream.consumeToken();
             EmojiPackage emojiPackage = getPackage();
             emojiClass = emojiPackage.classes.get(classIdentifierToken.toString());
             if (emojiClass == null)
-                throw new CompilerError(classIdentifierToken.startPosition, classIdentifierToken.endPosition, String.format("Class %s in package %s is not found", classIdentifierToken.toString(), emojiPackage.name));
+                throw new CompilerError(classIdentifierToken.startPosition,
+                                        classIdentifierToken.endPosition,
+                                        String.format("Class %s in package %s is not found",
+                                                      classIdentifierToken.toString(),
+                                                      emojiPackage.name));
             
-//DANIEL            System.out.format("AAAA Class: %s, method: %s, type: %s\n", emojiClass.name, methodName, methodType.name());
-//            System.out.format("AAAA Class: %s, method: %s, type: %s\n", emojiClass.name, emojiMethod.name, emojiMethod.methodType.name());
+//DANIEL            System.out.format("AAAA Class: %s, method: %s, type: %s\n",
+//DANIEL                              emojiClass.name, methodName, methodType.name());
+//            System.out.format("AAAA Class: %s, method: %s, type: %s\n",
+//                              emojiClass.name, emojiMethod.name, emojiMethod.methodType.name());
 //            System.out.flush();
             
-            if (methodType == EmojiMethod.MethodType.TYPE_METHOD)
-                emojiMethod = emojiClass.getTypeMethod(methodName, methodIdentifierToken.startPosition, classIdentifierToken.endPosition);
-            else
-                emojiMethod = emojiClass.getMethod(methodName, methodIdentifierToken.startPosition, classIdentifierToken.endPosition);
-            if (emojiMethod == null)
-                throw new CompilerError(methodIdentifierToken.startPosition, classIdentifierToken.endPosition, String.format("Method %s in class %s in package s is not found", methodName, classIdentifierToken.toString()));
+            if (methodType == EmojiMethod.MethodType.TYPE_METHOD) {
+                emojiMethod = emojiClass.getTypeMethod(methodName,
+                                                       methodIdentifierToken.startPosition,
+                                                       classIdentifierToken.endPosition);
+            } else {
+                emojiMethod = emojiClass.getMethod(methodName,
+                                                   methodIdentifierToken.startPosition,
+                                                   classIdentifierToken.endPosition);
+            }
+            if (emojiMethod == null) {
+                throw new CompilerError(methodIdentifierToken.startPosition,
+                                        classIdentifierToken.endPosition,
+                                        String.format(
+                                                "Method %s in class %s in package s is not found",
+                                                methodName,
+                                                classIdentifierToken.toString()));
+            }
             
             List<EmojiMethodArgumentType> argumentTypes = emojiMethod.argumentTypes;
             for (EmojiMethodArgumentType argumentType : argumentTypes) {
@@ -137,7 +174,11 @@ public final class MethodCall extends Parent {
             }
             
         } else {
-            throw new CompilerError(classIdentifierToken.startPosition, classIdentifierToken.endPosition, "Why did we get here??? TokenType: "+classIdentifierToken.type.name());
+            throw new CompilerError(classIdentifierToken.startPosition,
+                                    classIdentifierToken.endPosition,
+                                    "Why did we get here??? TokenType: "
+                                            + classIdentifierToken.type.name());
+            
 //            throw new CompilerError("Method call has no class instance");
         }
         
