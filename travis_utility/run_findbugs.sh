9de2518bbf3c79179966024b40709ab889a9aec4
Daniel <db123@bergqvist.se>
2017-12-04 10:18:56 +0100

    Configure Findbugs

--- travis_utility/run_findbugs.sh
+++ travis_utility/run_findbugs.sh
@@ -6,5 +6,5 @@ export FINDBUGS_HOME=./findbugs/findbugs-3.0.1
 
 mkdir ./build/findbugs
 
-java -jar $FINDBUGS_HOME/lib/findbugs.jar -textui -html -output ./build/findbugs/findbugs.html -sourcepath ./src -home $FINDBUGS_HOME ./build/classes
+java -classpath $FINDBUGS_HOME/lib -jar $FINDBUGS_HOME/lib/findbugs.jar -textui -html -output ./build/findbugs/findbugs.html -sourcepath ./src ./build/classes
 
