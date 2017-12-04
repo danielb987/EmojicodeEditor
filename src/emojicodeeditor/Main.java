009233d85ceb8f43e783ec6ff53167f3d0f77816
Daniel <db123@bergqvist.se>
2017-12-04 08:11:11 +0100

    Fix checkstyle errors in Main.java

--- src/emojicodeeditor/Main.java
+++ src/emojicodeeditor/Main.java
@@ -42,6 +42,8 @@ public final class Main {
     private Main() {
     }
     
+    //CHECKSTYLE.OFF: MagicNumberCheck - Magic numbers is used for the number of parameters that
+    // the program expects and there is no need to have constants for these.
     /**
      * The starting point of the program.
      *
@@ -104,10 +106,11 @@ public final class Main {
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and
          * feel. For details see
-         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            for (javax.swing.UIManager.LookAndFeelInfo info
+                    : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
@@ -130,5 +133,7 @@ public final class Main {
             }
         });
     }
+    //CHECKSTYLE.ON: MagicNumberCheck - Magic numbers is used for the number of parameters that
+    // the program expects and there is no need to have constants for these.
     
 }
