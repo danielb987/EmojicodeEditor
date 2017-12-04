fb367085377591a628cbd84728ad56e3aebf59a9
Daniel <db123@bergqvist.se>
2017-12-04 10:57:14 +0100

    Commit

--- travis_utility/run_findbugs.sh
+++ travis_utility/run_findbugs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export FINDBUGS_HOME=./findbugs/findbugs-3.0.1
+
+# export CLASSPATH=$FINDBUGS_HOME/lib/*:$FINDBUGS_HOME/apache-ant-1.10.1/lib/*
+export CLASSPATH=$FINDBUGS_HOME/lib:$FINDBUGS_HOME/apache-ant-1.10.1/lib
+
 echo -e ""
 echo -e "==============================="
 echo -e ""
@@ -19,7 +24,8 @@ export FINDBUGS_HOME=./findbugs/findbugs-3.0.1
 
 mkdir ./build/findbugs
 
-java -classpath "$FINDBUGS_HOME/lib/*:$FINDBUGS_HOME/apache-ant-1.10.1/lib/*" -jar $FINDBUGS_HOME/lib/findbugs.jar -textui -html -output ./build/findbugs/findbugs.html -sourcepath ./src ./build/classes ./lib
+java -jar $FINDBUGS_HOME/lib/findbugs.jar -textui -html -output ./build/findbugs/findbugs.html -sourcepath ./src ./build/classes ./lib
+# java -classpath "$FINDBUGS_HOME/lib/*:$FINDBUGS_HOME/apache-ant-1.10.1/lib/*" -jar $FINDBUGS_HOME/lib/findbugs.jar -textui -html -output ./build/findbugs/findbugs.html -sourcepath ./src ./build/classes ./lib
 
 # java -jar $FINDBUGS_HOME/lib/findbugs.jar -textui -html -output ./build/findbugs/findbugs.html -sourcepath ./src -home $FINDBUGS_HOME ./build/classes
 
