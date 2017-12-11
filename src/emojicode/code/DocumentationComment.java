41e7ae31f2082678a570596a7de6721e11ab9627
Daniel <db123@bergqvist.se>
2017-12-11 02:13:11 +0100

    Test checkstyle configuration

--- src/emojicode/code/DocumentationComment.java
+++ src/emojicode/code/DocumentationComment.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2017 Daniel Bergqvist.
+ * Copyright 2017 Daniel Bergqvsssist.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
