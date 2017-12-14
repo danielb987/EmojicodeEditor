f1066b7fff513d2777be9dc9802b865650c425f8
Daniel <db123@bergqvist.se>
2017-12-14 03:08:39 +0100

    Remove license headers

--- src/emojicode/code/EmojiClass.java
+++ src/emojicode/code/EmojiClass.java
@@ -1,26 +1,3 @@
-/*
- * The MIT License
- *
- * Copyright 2017 Daniel Bergqvist.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package emojicode.code;
 
 import emojicode.SourcePosition;
@@ -31,19 +8,17 @@ import java.util.Map;
 /**
  * EmojiClass
  * http://www.emojicode.org/docs/reference/classes-valuetypes.html
- * 
- * @author Daniel Bergqvist
  */
 public abstract class EmojiClass extends Parent {
     
     final String name;
     
-    private final Map<String, EmojiMethod> initializers = new HashMap<>();
-    private final Map<String, EmojiMethod> typeMethods = new HashMap<>();
-    private final Map<String, EmojiMethod> methods = new HashMap<>();
+    private final Map<String, EmojiMethod> fInitializers = new HashMap<>();
+    private final Map<String, EmojiMethod> fTypeMethods = new HashMap<>();
+    private final Map<String, EmojiMethod> fMethods = new HashMap<>();
     
     
-    public EmojiClass(String name, Parent parent) {
+    public EmojiClass(final String name, final Parent parent) {
         super(parent, Parent.HasVariables.YES);
         this.name = name;
     }
@@ -52,26 +27,27 @@ public abstract class EmojiClass extends Parent {
 //    public abstract EmojiClassInstance createInstance();
     
     
-    public final void addInitializerOrMethod(   EmojiMethod emojiMethod,
-                                                SourcePosition startPosition,
-                                                SourcePosition endPosition)
+    public final void addInitializerOrMethod(   final EmojiMethod emojiMethod,
+                                                final SourcePosition startPosition,
+                                                final SourcePosition endPosition)
             throws CompilerError {
         
-//        System.out.format("Class: %s, method: %s, type: %s\n", name, emojiMethod.name, emojiMethod.methodType.name());
+//        System.out.format("Class: %s, method: %s, type: %s\n",
+//                          name, emojiMethod.name, emojiMethod.methodType.name());
 //        System.out.flush();
         
         switch (emojiMethod.methodType) {
             case INITIALIZER:
             case REQUIRED_INITIALIZER:
-                initializers.put(emojiMethod.name, emojiMethod);
+                fInitializers.put(emojiMethod.name, emojiMethod);
                 break;
                 
             case METHOD:
-                methods.put(emojiMethod.name, emojiMethod);
+                fMethods.put(emojiMethod.name, emojiMethod);
                 break;
                 
             case TYPE_METHOD:
-                typeMethods.put(emojiMethod.name, emojiMethod);
+                fTypeMethods.put(emojiMethod.name, emojiMethod);
                 break;
                 
             default:
@@ -80,26 +56,49 @@ public abstract class EmojiClass extends Parent {
     }
     
     
-    public final EmojiMethod getInitializer(String methodName, SourcePosition startPosition, SourcePosition endPosition) throws CompilerError {
-        EmojiMethod method = initializers.get(methodName);
+    public final EmojiMethod getInitializer(final String methodName,
+                                            final SourcePosition startPosition,
+                                            final SourcePosition endPosition)
+            throws CompilerError {
+        
+        EmojiMethod method = fInitializers.get(methodName);
+        
+        if (method == null) {
+            throw new CompilerError(startPosition,
+                                    endPosition,
+                                    String.format("Method %s in class %s is not found",
+                                                  methodName,
+                                                  name));
+        }
+        
+        return method;
+    }
+    
+    
+    public final EmojiMethod getTypeMethod(final String methodName,
+                                           final SourcePosition startPosition,
+                                           final SourcePosition endPosition)
+            throws CompilerError {
+        
+        EmojiMethod method = fTypeMethods.get(methodName);
         if (method == null)
             throw new CompilerError(startPosition, endPosition, String.format("Method %s in class %s is not found", methodName, name));
         return method;
     }
     
     
-    public final EmojiMethod getTypeMethod(String methodName, SourcePosition startPosition, SourcePosition endPosition) throws CompilerError {
-        EmojiMethod method = typeMethods.get(methodName);
-        if (method == null)
-            throw new CompilerError(startPosition, endPosition, String.format("Method %s in class %s is not found", methodName, name));
-        return method;
-    }
-    
-    
-    public final EmojiMethod getMethod(String methodName, SourcePosition startPosition, SourcePosition endPosition) throws CompilerError {
-        EmojiMethod method = methods.get(methodName);
-        if (method == null)
-            throw new CompilerError(startPosition, endPosition, String.format("Method %s in class %s is not found", methodName, name));
+    public final EmojiMethod getMethod(final String methodName,
+                                       final SourcePosition startPosition,
+                                       final SourcePosition endPosition)
+            throws CompilerError {
+        
+        EmojiMethod method = fMethods.get(methodName);
+        if (method == null) {
+            throw new CompilerError(startPosition,
+                                    endPosition,
+                                    String.format("Method %s in class %s is not found",
+                                                  methodName, name));
+        }
         return method;
     }
     
